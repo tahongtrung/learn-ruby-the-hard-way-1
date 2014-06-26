b8fd40399c69358912144e84e8e06a78d36ed0d8
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-06-25 21:27:14 -0600

    Add Chapter 5
      *Completed the exercises in Chapter 5
    
      *Finished extra credit

---
+++ Chapter_05/ex5x2.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+inch = 1.0
+centimeters_in_inch = 2.54
+foot_in_inches = inch * 12.0
+foot_in_centimeters = centimeters_in_inch * foot_in_inches
+
+puts "There are #{foot_in_centimeters} centimeters in a foot."
+
+kilograms_in_a_pound = 0.453592
+
+puts 'There are %f kilograms in 10,000 pounds.' %
+       (10000.0 * kilograms_in_a_pound)
+
+puts 'There are %g centimeters in an inch and %g kilograms in a pound.' %
+       [centimeters_in_inch, kilograms_in_a_pound]
