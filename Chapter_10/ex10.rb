91962adc1ebe52e418d26271f86f3d3f728ed6cb
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-06-28 17:23:11 -0600

    Copy extra credit answer to ex10.rb
      * copy and pasted the answer to 2. of the extra credit
    	from my_ex10.rb to the last line of ex10.rb

--- Chapter_10/ex10.rb
+++ Chapter_10/ex10.rb
@@ -15,3 +15,23 @@ puts tabby_cat
 puts persian_cat
 puts backslash_cat
 puts fat_cat
+
+# extra credit
+puts ''
+# 2.
+
+tabby_cat = "\t\tI'm %s in %s." % ['tabbed', 'twice']
+persian_cat = "I\nsplit\non\nwords."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = <<MY_HEREDOC
+I'll do a list:
+\t* Cat food \\
+\t\t* Fishies \\
+\t\t\t* C\sa\st\sn\si\sp\\\n\t\t\t\t* Grass \\
+MY_HEREDOC
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
