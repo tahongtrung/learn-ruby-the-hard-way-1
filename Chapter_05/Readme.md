216e1fac0887302f8f35b4078b8e4033c177f72e
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-06-28 15:47:46 -0600

    Rename file
      * renamed extra credit file to my_ex5.rb and moved
    	all extra credit answers thereee
    
      * put rubocop info in Readme.md

---
+++ Chapter_05/Readme.md
@@ -0,0 +1,3 @@
+The rubocop errors were format sequence offenses, so they weren't fixed. Rubocop
+also suggested to use %w format sequence when dealing with word instead of numbers,
+but it didn't work when I tried to substitute it. I ignored that error as well.
--- Chapter_05/my_ex5.rb
+++ Chapter_05/my_ex5.rb
@@ -1,4 +1,9 @@
 #!/usr/bin/env ruby
+
+# extra credit
+
+# 1
+
 name = 'Zed A. Shaw'
 age = 35 # not a lie
 height = 74 # inches
@@ -17,3 +22,22 @@ puts 'His teeth are usually %s depending on the coffee.' % teeth
 # this line is tricky, try to get it exactly right
 puts 'If I add %d, %d, and %d I get %d.' % [
   age, height, weight, age + height + weight]
+
+# my format sequences
+puts ''
+# 2.
+
+inch = 1.0
+centimeters_in_inch = 2.54
+foot_in_inches = inch * 12.0
+foot_in_centimeters = centimeters_in_inch * foot_in_inches
+
+puts "There are #{foot_in_centimeters} centimeters in a foot."
+
+kilograms_in_a_pound = 0.453592
+
+puts 'There are %f kilograms in 10,000 pounds.' %
+       (10_000.0 * kilograms_in_a_pound)
+
+puts 'There are %g centimeters in an inch and %g kilograms in a pound.' %
+       [centimeters_in_inch, kilograms_in_a_pound]
