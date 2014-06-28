4d38e69e14ff43ad28938b6128f3bd277a0e4261
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-06-28 13:36:50 -0600

    Remove rubocop offense
      * removed 'string too long' offense by splitting the
        string in two and adding them together

--- Chapter_03/my_calculation.rb
+++ Chapter_03/my_calculation.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
 
 # calculating how many times I have walked my dog
-puts 'The following is how many times I walked my dog: %d times.' % [365.25 * 15 * 3]
+puts 'The following is how many times I walked my dog:' +
+       ' %d times.' % [365.25 * 15 * 3]
