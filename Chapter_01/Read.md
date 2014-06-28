a3ec2d3db3cdd1f69e136d986328d4077a32e419
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-06-28 11:53:28 -0600

    Changes to Chapter 1
      * Changed double qouted strings back to single quotes,
    that could be changed, to clear rubocop offences
    
      * Put extra credit answers in ex1.rb per Chapter 1 Pivitol
    Tracker story.

--- Chapter_01/Read.md
+++ Chapter_01/Read.md
@@ -1,5 +1,10 @@
 The offenses were fixed by changing the double quotes to 
-single quotes for the string that didn't have single quotes 
+single quotes for the strings that didn't have single quotes 
 inside them. This action was chosen because
 it is convention to use single quotes if double quotes 
 aren't required, like when using string interpolation.
+I changed the strings back after the offenses were fixed to
+match the exercise in Learn Ruby the Hard Way, to stay with 
+convention.
+
+
--- Chapter_01/ex1.rb
+++ Chapter_01/ex1.rb
@@ -1,7 +1,45 @@
-puts "Hello World!"
-puts "Hello Again"
-puts "I like typing this."
-puts "This is fun"
+puts 'Hello World!'
+puts 'Hello Again'
+puts 'I like typing this.'
+puts 'This is fun'
 puts 'Yay! Printing.'
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
+
+puts ''
+puts 'extra credit 1.'
+puts ''
+
+puts 'Hello World!'
+puts 'Hello Again'
+puts 'I like typing this.'
+puts 'This is fun'
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts ''
+
+puts ''
+puts 'extra credit 2.'
+puts ''
+puts 'Hello World!'
+# puts 'I like typing this.'
+# puts 'This is fun'
+# puts 'Yay! Printing.'
+# puts "I'd much rather you 'not'."
+# puts 'I "said" do not touch this.'
+# puts ''
+
+puts ''
+puts 'extra credit 3.'
+puts ''
+# puts 'Hello World!'
+# puts 'I like typing this.'
+# puts 'This is fun'
+# puts 'Yay! Printing.'
+# puts "I'd much rather you 'not'."
+# puts 'I "said" do not touch this.'
+# puts ''
+
+puts 'The pound sign comments out the code so it will
+      not be executed'
